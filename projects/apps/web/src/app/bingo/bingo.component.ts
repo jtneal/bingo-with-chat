21a2510064b0a95abbedd01b64ecd4f8d8274922
Jason Neal <5340961+jtneal@users.noreply.github.com>
2024-01-06 14:53:29 -0500

    Lots of great improvements

--- projects/apps/web/src/app/bingo/bingo.component.ts
+++ projects/apps/web/src/app/bingo/bingo.component.ts
@@ -13,9 +13,11 @@ import { AlignmentHandler } from './alignment.handler';
 
 /**
  * Button to save
- * Auto bingo key hex
+ * Auto bingo key hex / uuid
  * Anyone that loads bingo key gets random card but refreshing keeps the card and all in progress stuff.
- * credit at bottom to HochiDev
+ * Have to be logged in to play, no way around it and limits people to 1 card and no cheating
+ * Login with twitch, kick, youtube, etc.
+ * Regular players instead of textarea get a text only box or uneditable textarea when clicked creates an X w/ $blueFade
  */
 
 const mock =
@@ -38,11 +40,41 @@ export class BingoComponent implements AfterViewInit, OnDestroy {
   private readonly destroy$ = new Subject<void>();
 
   public card = [
-    ['', '', '', '', mock],
-    ['', '', '', '', ''],
-    ['', '', 'FREE!', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
+    [
+      'F Bomb Dropped',
+      'Show starts late',
+      'BG3 wins an award',
+      'Destiny wins an award',
+      mock,
+    ],
+    [
+      'Show ends late',
+      'Technical difficulties',
+      "Crowd boo's",
+      'Wrap up music plays during thank you speech',
+      'Someone trips and falls down',
+    ],
+    [
+      'Unexpected celeb comes on stage',
+      'Someone sneezes on stage',
+      'FREE!',
+      mock,
+      'Half Life 3 announced',
+    ],
+    [
+      'Destiny 3 announced',
+      'Super Cringe',
+      'Someone talks with gum in their mouth',
+      'Someone wearing pajamas',
+      'Pedro Eustache is shown jamming out on an instrument',
+    ],
+    [
+      'Thank you speech is cut off after being far too long',
+      mock,
+      'Aliens or UFOs are mentioned',
+      'War/genocide is mentioned',
+      'Layoffs are mentioned',
+    ],
   ];
 
   public ngAfterViewInit(): void {
